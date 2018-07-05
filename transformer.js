2f5534d4fc523e01a379ea1aa2ff35a12629c6db
arjd4science <adornford@tetrascience.com>
2018-07-05 12:33:22 -0400

    splat

--- transformer.js
+++ transformer.js
@@ -8,16 +8,12 @@
  @param {Object} logData.meta - the log meta data (JSON object)
  @returns {Object} transformed message
  */
-const isObject = (obj) => { return obj instanceof Object; };
-
 const transformer = function transformer(logData) {
   const transformed = {};
   transformed['@timestamp'] = logData.timestamp ? logData.timestamp : new Date().toISOString();
   transformed.message = logData.message;
   transformed.severity = logData.level;
-  transformed.fields = (isObject(logData.meta) && logData.meta)
-    || (logData.meta && { meta: logData.meta });
-  console.log(JSON.stringify(transformed));
+  transformed.fields = logData.meta;
   return transformed;
 };
 
