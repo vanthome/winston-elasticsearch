9b69dfd3c11ff5acce8c98301c0fa9f7521f4f74
MichaelTsafrir <michaeltsafrir@gmail.com>
2018-07-18 17:19:19 +0300

    Create index.d.ts
    TypeScript declerations

---
+++ index.d.ts
@@ -0,0 +1,41 @@
+declare module 'winston-elasticsearch' {
+	import TransportStream from 'winston-transport';
+	import elasticsearch from 'elasticsearch';
+
+	export interface LogData {
+		message: any;
+		level: string;
+		meta: { [key: string]: any };
+		timestamp?: string;
+	}
+
+	export interface Transformer {
+		(logData: LogData): any;
+	}
+
+	export interface ElasticsearchTransportOptions extends TransportStream.TransportStreamOptions {
+		timestamp?: () => string;
+		level?: string;
+		index?: string;
+		indexPrefix?: string;
+		indexSuffixPattern?: string;
+		messageType?: string;
+		transformer?: Transformer;
+		mappingTemplate?: { [key: string]: any };
+		ensureMappingTemplate?: boolean;
+		flushInterval?: number;
+		waitForActiveShards?: number;
+		handleExceptions?: boolean;
+		pipeline?: string;
+		client?: elasticsearch.Client;
+		clientOpts?: elasticsearch.ConfigOptions;
+	}
+
+	export default class Elasticsearch extends TransportStream {
+		constructor(opts?: ElasticsearchTransportOptions);
+
+		query<T>(options: any, callback?: () => void): Promise<SearchResponse<T>>;
+		query<T>(q: string): Promise<SearchResponse<T>>;
+		getIndexName(opts: ElasticsearchTransportOptions): string;
+	}
+}
